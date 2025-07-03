e2e0ede683de03a82941c3d0cab108786553c55c
Omarkhodjaev <umarkhankhodjaev@gmail.com>
2025-07-03 17:11:59 +0500

    ADD CI/CD

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'express-app',
+      script: 'dist/index.js',
+      instances: 1,
+      exec_mode: 'fork',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3000,
+      },
+    },
+  ],
+};
